d39cafcb783b47341f57fd4be791cf3a95b9cbdf
alejandrorv68 <31384598+alejandrorv68@users.noreply.github.com>
2019-05-06 22:17:44 +0200

    Add files via upload

---
+++ attinyDistanceSense.ino
@@ -0,0 +1,33 @@
+/*
+ * UltrasonicSensor+LED+attiny85
+ * https://create.arduino.cc/projecthub/arjun/programming-attiny85-with-arduino-uno-afb829
+ * Ultrasonic sensor HC-SR04 works on 5V 
+ * https://create.arduino.cc/projecthub/MisterBotBreak/how-to-use-an-ultrasonic-sensor-181cee
+*/
+
+int trig = 1; 
+int echo = 2; 
+int led = 0;
+long lecture_echo; 
+long cm;
+void setup() 
+{ 
+  pinMode(trig, OUTPUT); 
+  pinMode(led, OUTPUT); 
+  digitalWrite(trig, LOW); 
+  pinMode(echo, INPUT); 
+  //Serial.begin(9600); 
+}
+void loop() 
+{ 
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(trig, LOW); 
+  lecture_echo = pulseIn(echo, HIGH); 
+  cm = lecture_echo / 58; 
+  //Serial.print("Distance in cm : "); 
+  //Serial.println(cm); 
+  if(cm<10){digitalWrite(led,HIGH);}
+  else{digitalWrite(led,LOW);}
+  delay(1000); 
+}
